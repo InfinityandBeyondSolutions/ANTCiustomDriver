d1fbcf57b09ab375b8d3d9aeb5da915152f7b2fb
Daniel <danielantonie03@gmail.com>
2026-03-04 20:16:47 +0200

    Bump version code and name to 3.0 for release preparation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.ibs.ibs_antdrivers"
         minSdk = 27
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
